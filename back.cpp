985e33ae768364d84a0bb8da5bfde127cb61c41f
Ahsan Arafat <oytizo.bd@gmail.com>
2022-10-04 13:23:38 +0600

    comment remove and added to beside variable

--- back.cpp
+++ back.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 int main(){
 
-    int a=80;
+    int a=80;  //this testing purpose
     int b=90;
     int c;
     int d=9;
-//this is informative
     c=a+b+d;
     cout<<c;
 }
