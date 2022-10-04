38762590d1c9732877ba59ed8183d9384406a99e
Ahsan Arafat <oytizo.bd@gmail.com>
2022-10-04 12:37:49 +0600

    added comment and variable modify

--- back.cpp
+++ back.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main(){
 
-    int a=8;
-    int b=9;
+    int a=80;
+    int b=90;
     int c;
-    int d=90;
+    int d=9;
+//this is informative
     c=a+b+d;
     cout<<c;
 }
